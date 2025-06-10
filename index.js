5938b848e767fb1dffa31a0031005a8f670ff749
edem-sed <edemsedzoame@gmail.com>
2025-06-10 21:50:33 +0000

    add some

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express"
 import { booksRouter } from "./routes/library_routes.js";
 import mongoose from "mongoose";
 import "dotenv/config"
-import cors from cors
+import cors from "cors"
 
 const app = express ()
 
