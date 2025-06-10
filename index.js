da691031534b883982aff5c90dceb3939067033e
edem-sed <edemsedzoame@gmail.com>
2025-06-10 21:43:45 +0000

    added api/v1

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 7788
 app.use(express.json())
 app.use(cors())
 
-app.use(booksRouter)
+app.use("/api/v1", booksRouter)
 const mongoURI = process.env.MONGO_URI;
 
 
